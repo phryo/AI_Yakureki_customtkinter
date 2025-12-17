06115c8b046c60caf0f39740a5868e6804eec051
ryo <pyryo1230@gmail.com>
2025-12-17 23:16:56 +0900

    dir指定変更

--- settings/setting.py
+++ settings/setting.py
@@ -2,14 +2,20 @@ import platform
 from pathlib import Path
 
 
-if platform.system() == "Windows":
-    BASE_DIR = Path(r"\\Toridesvr01\取手\06_マニュアル・ 書類フォーマット\AI-薬歴")
-else:
-    BASE_DIR = Path(__file__).resolve().parent.parent
-
-# BASE_DIR = Path(r"C:\Users\sakur\PycharmProjects\AI_Yakureki_customtkinter")
+# if platform.system() == "Windows":
+#     BASE_DIR = Path(r"\\Toridesvr01\取手\06_マニュアル・ 書類フォーマット\AI-薬歴")
+# else:
+#     BASE_DIR = Path(__file__).resolve().parent.parent
+#
+# # BASE_DIR = Path(r"C:\Users\sakur\PycharmProjects\AI_Yakureki_customtkinter")
 
 # db_operation.py
+DIR_PATH = 'データ保管場所.txt'
+try:
+    with open(DIR_PATH, 'r', encoding='utf-8') as f:
+        BASE_DIR = Path(f.read())
+except FileNotFoundError:
+    BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PATH = BASE_DIR / 'summaries.db'
 PROMPT_PATH = BASE_DIR / 'prompt.txt'
 
